ef4b5398be78420014a88fcc4d510d62d117333d
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2023-12-14 13:52:03 +0500

    hello

--- project/user.py
+++ project/user.py
@@ -2,33 +2,31 @@ from fastapi import APIRouter, HTTPException
 from .schemas import enter_to_test, accept_score_schema
 from fastapi_sqlalchemy import db
 from .models import Students, Login_code
-
+from datetime import datetime
 
 router=APIRouter(prefix="/user")
 
 
 @router.post("/enter_to_test")
 def enter_to_test(user_schema:enter_to_test):
-    check_login_code=db.session.query(Login_code).first()
+    check_login_code=db.session.query(Login_code).filter_by(login_code=user_schema.login_code).first()
     if check_login_code:
-        if check_login_code.login_code==user_schema.login_code:
-            new_teacher=Students(
-                name=user_schema.name,
-                surname=user_schema.surname,
-                login_code=user_schema.login_code,
-                score=0
-            )
-            db.session.add(new_teacher)
-            db.session.commit()
-            return {
-                "id":new_teacher.id,
-                "time":check_login_code.expired_time,
-                "word_box":check_login_code.word_box
-            }
-        else:
-            raise HTTPException(status_code=404,detail="Invalid login code")
+        new_teacher=Students(
+            name=user_schema.name,
+            surname=user_schema.surname,
+            login_code=user_schema.login_code,
+            score=0,
+            registered_time=datetime.now()
+        )
+        db.session.add(new_teacher)
+        db.session.commit()
+        return {
+            "id":new_teacher.id,
+            "time":check_login_code.expired_time,
+            "word_box":check_login_code.word_box
+        }
     else:
-        raise HTTPException(status_code=404,detail="No exam found now")
+        raise HTTPException(status_code=404,detail="No exam found with this logincode")
 
 
 @router.put("/accept_score")
